0f55f922fa1814b56e44aa0949308a85791c1892
Elena Andrakaki <elena.andrakaki@outlook.com>
2023-03-25 12:56:17 +0100

    first commit

---
+++ Dandraka.HeifConvert/Dandraka.HeifConvert.Converter/ImgConvert.cs
@@ -0,0 +1,45 @@
+﻿using ImageMagick;
+
+namespace Dandraka.HeifConvert.Converter
+{
+    public enum ImageType
+    {
+        Png,
+        Jpg,
+        Gif,
+        Tiff,
+        Bmp
+    }
+    public static class ImgConvert
+    {
+        public static void ConvertFile(string inputfilename, ImageType? convertType = ImageType.Jpg, string? outputfilename = null)
+        {
+            if (string.IsNullOrEmpty(outputfilename))
+            {
+                string dir = Path.GetDirectoryName(inputfilename);
+                outputfilename = Path.Combine(dir, Path.ChangeExtension(inputfilename, convertType.ToString()));
+            }
+            using (var image = new MagickImage(inputfilename))
+            {
+                image.Write(outputfilename);
+            }
+        }
+
+        public static void ConvertFiles(List<string> inputfilenameList)
+        {
+            foreach (string inputfilename in inputfilenameList)
+            {
+                ConvertFile(inputfilename);
+            }
+        }
+
+        public static void ConvertDir(string inputDir, string filter)
+        {
+            var list = Directory.GetFiles(inputDir, filter);
+            foreach (string inputfilename in list)
+            {
+                ConvertFile(inputfilename);
+            }
+        }
+    }
+}
---
+++ Dandraka.HeifConvert/Dandraka.HeifConvert.Tests/ImgConvertTests.cs
@@ -0,0 +1,11 @@
+namespace Dandraka.HeifConvert.Tests
+{
+    public class ImgConvertTests
+    {
+        [Fact]
+        public void T01_ConvertToJpeg()
+        {
+
+        }
+    }
+}
