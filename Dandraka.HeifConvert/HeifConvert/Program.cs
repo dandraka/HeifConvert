057f126897edd121bee551eb22214b6bd45bd8b6
Jim <dandraka@gmail.com>
2023-05-03 20:06:17 +0200

    parsing of cmd line args

--- Dandraka.HeifConvert/HeifConvert/Program.cs
+++ Dandraka.HeifConvert/HeifConvert/Program.cs
@@ -1,6 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Dandraka.HeifConvert.Converter;
+using Fclp;
+
+string path = "";
+ImageType imgType = ImageType.jpg;
+getCommandlineArgs(args);
+
+void getCommandlineArgs(string[] a)
+{
+    var p = new FluentCommandLineParser();
+    p.Setup<string>('p', "Path")
+        .WithDescription("Path to a HEIF/HEIC file or directory containing HEIF/HEIC files.")
+        .Callback(x => path = x)
+        .Required();
+    p.Setup<ImageType>('o', "OutputType")
+        .WithDescription("Supported output types: " + string.Join(',', Enum.GetNames(typeof(ImageType))))
+        .Callback(x => imgType = x)
+        .SetDefault(ImageType.jpg);
+    //p.SetupHelp()
+
+    p.Parse(a);
+}
+
+//sanity checks
+if (string.IsNullOrWhiteSpace(path))
+{
+    throw new ArgumentException("Path (-p or -Path) must be specified.");
+}
+if (!File.Exists(path) && !Directory.Exists(path))
+{
+    throw new DirectoryNotFoundException(path);
+}    
 
 // convert heic/heif to jpg
-string dir = args[0];
-ImgConvert.ConvertDir(dir);
+if (Directory.Exists(path)) 
+{
+    ImgConvert.ConvertDir(path);
+}
+else
+{
+    ImgConvert.ConvertFile(path);
+}
